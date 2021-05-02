ad83fd5c2529dc72fe5dc4961cb1669660404de2
timmeno1 <stimka91@gmail.com>
2021-05-02 09:11:55 +0900

    DetailPage and LinkCard

---
+++ client/src/components/LinkCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const LinkCard = ({link}) => {
+    return (
+        <>
+            <h3>Ссылка</h3>
+            <p>Ваша ссылка: <a
+                href={link.to}
+                target={'_blank'}
+                rel={'noopener noreferrer'}
+            >{link.to}</a></p>
+            <p>Откуда: <a
+                href={link.from}
+                target={'_blank'}
+                rel={'noopener noreferrer'}
+            >{link.from}</a></p>
+            <p>Количество кликов: <strong>{link.clicks}</strong> </p>
+            <p>Дата создания: <strong>{new Date(link.date).toLocaleDateString()}</strong> </p>
+        </>
+    )
+}
--- routes/link.routes.js
+++ routes/link.routes.js
@@ -43,9 +43,8 @@ router.get('/', auth, async (req,res) => {
     }
 })
 
-router.get('/detail', auth, async (req,res) => {
+router.get('/:id', auth, async (req,res) => {
     try {
-        console.log("kutak")
         const link = await Link.findById(req.params.id) //??
         res.json(link)
     } catch (e) {
